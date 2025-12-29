d2da3035179496e9d5928a97285e02699bdc1498
noor52 <nooralam52cse@gmail.com>
2025-12-29 14:42:36 +0600

    cimmit

--- project 2/backend/src/main/java/com/noor170/culturalpro/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.noor170.culturalpro.repository;
-
-import com.noor170.culturalpro.model.OrderEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
-}
